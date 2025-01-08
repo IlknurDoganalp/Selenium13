827d8fdec9dfe1d1ab7302ecf9bfc70ba31f3c97
IlknurDoganalp <128212123+IlknurDoganalp@users.noreply.github.com>
2025-01-08 12:56:09 +0300

    Selenium-V1

---
+++ src/test/java/day01_driverOlusturma/C02_Main.java
@@ -0,0 +1,19 @@
+package day01_driverOlusturma;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_Main {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+
+    }
+}
